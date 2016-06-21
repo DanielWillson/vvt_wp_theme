0a7a79b77cbaf226ab6edc9db2c528de9ae9959e
mckorbon <bookertee2@gmail.com>
2016-06-21 17:43:24 -0400

    Initial work on video background

--- parts/hero.php
+++ parts/hero.php
@@ -1,5 +1,32 @@
-<div class="fw-container hero clear">
-    <div class="video-container">
+
+<div id="event-video-teaser" class="fw-container hero clear">
+	<div class="videoWrapper">
+	        <iframe id="ytplayer" type="text/html"
+	src="<?php echo get_template_directory_uri (); ?>/images/website_vid_color.mp4"
+	frameborder="0" allowfullscreen></iframe>
+	</div>
+		<script>
+
+			/* Default */
+	        $(function() {
+	            console.log((16 / 9) * $("#fw-container").width());
+	            $('.videoWrapper').css('height', (9 / 16) * 1320);
+	        });
+
+	        $(function() {
+	            $(window).resize(function() {
+	                if ($(window).width() < 1356) {
+	                	$('.videoWrapper').css('height', (9 / 16) * $(window).width());
+	                }
+	                else {
+	                	$('.videoWrapper').css('height', (9 / 16) * 1320);
+	                }
+	            });
+	        });
+	    </script>
+	    <div class="video-overlay">
+	    	This is an overlay
+	    </div>
 		<div class="hero-text">
 			<h3>
 				We’re on a mission to find, celebrate, and support Virginia’s top startups. 
@@ -8,4 +35,3 @@
 			<p>This September, the Virginia Velocity Tour will travel the Commonwealth to recruit, support, and celebrate the entrepreneurs building Virginia’s 21st century economy. It’s a jam-packed week of tours, talks, and pitch competitions with over $100,000 in prizes.</p>
 		</div>
 	</div>
-</div>
