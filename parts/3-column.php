d47ee856383e30fe9fc532af39aef3604bc7bb48
mckorbon <bookertee2@gmail.com>
2016-06-21 15:44:44 -0400

    Matt finished draft of 3-column

---
+++ parts/3-column.php
@@ -0,0 +1,26 @@
+<div class="fw-container">
+	<div class="3-column">
+		<div class="column">
+			<img src="<?php echo get_template_directory_uri (); ?>/images/image1.jpg">
+			<h3>Startups</h3>
+			<br>
+			<h4>Apply to be a competing startup in the Virginia Velocity Tour</h4>
+			<p>Startups gain acces to our investor network, press, and more. Get details and apply here.</p>
+		</div>
+		<div class="column">
+			<img src="<?php echo get_template_directory_uri (); ?>/images/image1.jpg">
+			<h3>Investors</h3>
+			<br>
+			<h4>Looking to invest in a Virginia Startup?</h4>
+			<br>
+			<p>Learn about those participating in the Virginia Velocity Tour</p>
+		</div>
+		<div class="column">
+			<img src="<?php echo get_template_directory_uri (); ?>/images/image1.jpg">
+			<h3>Community</h3>
+			<br>
+			<h4>Schedule and details about the tour will be available</h4>
+			<p>Sign up for updates</p>
+		</div>
+	</div>
+</div>
