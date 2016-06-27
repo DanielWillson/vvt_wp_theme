1b95cbabc06dafbd4d2496fb1a5b0f7bfb7a52e6
Daniel Willson <daniel.willson3@gmail.com>
2016-06-27 11:21:10 -0400

    changed colors of host section, adjusted headings and line heights

--- parts/image-text.php
+++ parts/image-text.php
@@ -1,10 +1,10 @@
 <div class="image-text">
 	<div class="fw-container">
 		<div class="image-left">
-            <img src="<?php echo get_template_directory_uri (); ?>/images/host-logos.jpg">
+            <img src="<?php echo get_template_directory_uri (); ?>/images/host-logos.png">
 		</div>
 		<div class="text-right">
-			<h3>About the Hosts of the Virginia Velocity Tour</h3>
+			<h2>About the Hosts of the Virginia Velocity Tour</h2>
 			<p>The tour is planned in partnership with <a href="https://www.virginia.gov/" target="_blank">Virginia Governor Terry McAuliffe</a>; <a href="https://commerce.virginia.gov/" target="_blank">Secretary of Commerce and Trade Maurice Jones</a>; Virginia Velocity; <a href="http://vilcap.com" target="_blank">Village Capital</a>; and local partners across the Commonwealth.</p>
 			<p>If you’re interested in becoming a stop, partner, or sponsor of the tour, email contact@virginiavelocity.org</p>
 		</div>
--- parts/video-text.php
+++ parts/video-text.php
@@ -7,7 +7,7 @@
 			</div>
 		</div>
 			<div class="text-right">
-				<h3 class="heading">The Virginia Velocity Tour</h3>
+				<h2 class="heading">The Virginia Velocity Tour</h2>
 				<p>In the past two decades, accelerated innovation, demographic shifts, and globalization have transformed our nation’s economy and redefined opportunity in the American workplace. Virginia has the opportunity to stay in front of these trends by supporting entrepreneurs that will drive innovation and produce jobs in energy, health, education, agriculture, financial inclusion and other sectors that will thrive in the 21st century.</p>
 				<p>This September 19-23, the Virginia Velocity Tour will visit cities across the Commonwealth to find, celebrate, and support these entrepreneurs. Here’s how you can participate.</p>
 			</div>
