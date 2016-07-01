aab1aa8331f2f9e0975b6ea7d123c0fd644e7e95
Daniel Willson <daniel.willson3@gmail.com>
2016-07-01 09:34:33 -0400

    Added Gilliam Family to sponsors

--- footer.php
+++ footer.php
@@ -14,6 +14,11 @@
 				<h2>Virginia Velocity Tour Sponsors</h2>
 				<ul class="sponsors">
 					<li class="sponsor">
+						<a href="http://www.thegilliamfoundation.org/" target="_blank">
+							<img src="<?php echo get_template_directory_uri (); ?>/images/gilliam.png" />
+						</a>
+					</li>
+					<li class="sponsor">
 						<a href="https://www.dom.com/corporate/our-commitments/community/charitable-giving-and-the-dominion-foundation" target="_blank">
 							<img src="<?php echo get_template_directory_uri (); ?>/images/dominion.png" />
 						</a>
