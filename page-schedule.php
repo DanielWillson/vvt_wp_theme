68fcefccd1f7ada1cbe457dbdbad7cc5052741b5
Daniel Willson <daniel.willson3@gmail.com>
2016-08-29 08:47:02 -0400

    finished up the schedule page

---
+++ page-schedule.php
@@ -0,0 +1,71 @@
+<?php /* Template Name: Schedule Page */ ?>
+
+<?php get_header(); 
+
+?>
+<div class="schedule-page" >
+	<div class="fw-container">
+		<ul>
+			<a href="http://virginiavelocitytour.org/roanoke-and-blacksburg/">
+				<li style="background: url('<?php echo get_template_directory_uri ();?>/images/roanoke.jpg') top center; -webkit-background-size: cover; -moz-background-size: cover; -o-background-size: cover; background-size: cover;">
+					<div class="filter"></div>
+					<div class="item-content">
+						<h3>Mon Sep 19:</h3>
+						<h1>Roanoke and Blacksburg</h1>
+						<h4>View Full Schedule &rarr;</h4>
+					</div>
+				</li>
+			</a>
+			<a href="http://virginiavelocitytour.org/richmond/">
+				<li style="background: url('<?php echo get_template_directory_uri ();?>/images/richmond.jpg') top center; -webkit-background-size: cover; -moz-background-size: cover; -o-background-size: cover; background-size: cover;">
+					<div class="filter"></div>
+					<div class="item-content">
+						<h3>Tues Sep 20:</h3>
+						<h1>Richmond</h1>
+						<h4>View Full Schedule &rarr;</h4>
+					</div>
+				</li>
+			</a>
+			<a href="http://virginiavelocitytour.org/hampton-roads/">
+				<li style="background: url('<?php echo get_template_directory_uri ();?>/images/virginia-beach.jpg') top center; -webkit-background-size: cover; -moz-background-size: cover; -o-background-size: cover; background-size: cover;">
+					<div class="filter"></div>
+					<div class="item-content">
+						<h3>Wed Sep 21:</h3>
+						<h1>Hampton Roads</h1>
+						<h4>View Full Schedule &rarr;</h4>
+					</div>
+				</li>
+			</a>
+			<a href="http://virginiavelocitytour.org/northern-virginia/">
+				<li style="background: url('<?php echo get_template_directory_uri ();?>/images/nova.jpg') top center; -webkit-background-size: cover; -moz-background-size: cover; -o-background-size: cover; background-size: cover;">
+					<div class="filter"></div>
+					<div class="item-content">
+						<h3>Thurs Sep 22:</h3>
+						<h1>Northern Virginia</h1>
+						<h4>View Full Schedule &rarr;</h4>
+					</div>
+				</li>
+			</a>
+			<a href="http://virginiavelocitytour.org/charlottesville/">
+				<li style="background: url('<?php echo get_template_directory_uri ();?>/images/cville.jpg') top center; -webkit-background-size: cover; -moz-background-size: cover; -o-background-size: cover; background-size: cover;">
+					<div class="filter"></div>
+					<div class="item-content">
+						<h3>Mon Sep 23:</h3>
+						<h1>Charlottesville</h1>
+						<h4>View Full Schedule &rarr;</h4>
+					</div>
+				</li>
+			</a>
+			<a href="http://virginiavelocity.us11.list-manage.com/subscribe?u=9b6739a967b12786ac03a8aef&id=1f058fef1b" target="_blank">
+				<li style="background: url('<?php echo get_template_directory_uri ();?>/images/startup.jpg') top center; -webkit-background-size: cover; -moz-background-size: cover; -o-background-size: cover; background-size: cover;">
+					<div class="filter"></div>
+					<div class="item-content">
+						<h1>Sign Up For Updates</h1>
+						<h4>Subscribe &rarr;</h1>
+					</div>
+				</li>
+			</a>
+		</ul>
+	</div>
+</div>
+<?php get_footer(); ?>
